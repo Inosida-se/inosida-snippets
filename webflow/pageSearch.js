48308c18c363adeb7cd8e04177f1049cbe9870b6
augustmarowski <august@inosida.se>
2022-05-31 15:40:49 +0200

    updated pagesearch

--- webflow/pageSearch.js
+++ webflow/pageSearch.js
@@ -3,6 +3,7 @@ class Search {
         this.itemList = typeof itemList === 'string' ? document.querySelector(itemList) : itemList;
         this.listItems = this.itemList.querySelectorAll(itemSelector);
         this.input = null;
+        this.label = null;
     }
     search(query) {
         if (this.input) {
@@ -11,6 +12,11 @@ class Search {
         
         let filter, itemName, i, txtValue;
         filter = query.toUpperCase();
+
+        if (this.label) {
+            this.label.innerText = filter === '' ? 'Alla' : query;
+        }
+
         for (i = 0; i < this.listItems.length; i++) {
             itemName = listItems[i].querySelector('[data-name="innerText"]');
             txtValue = itemName.textContent || itemName.innerText;
@@ -26,6 +32,12 @@ class Search {
         this.input = typeof input === 'string' ? document.querySelector(input) : input;
         this.input.addEventListener('keyup', () => {
             search(this.input.value);
+        });
+    }
+    addSearchLabel(label) {
+        this.label = typeof label === 'string' ? document.querySelector(label) : label;
+        this.label.addEventListener('click', () => {
+            this.input.focus();
         });
     }
 }
